1f87771f00533bb235b79cb2163e70b43fff3757
Yusuke <yskamto1024@gmail.com>
2022-12-22 15:14:53 +0900

    add parallel problem

--- Other/Parallization/parallelization_calc.py
+++ Other/Parallization/parallelization_calc.py
@@ -1,21 +1,95 @@
-from multiprocessing import Process, Queue, Pool, Manager
+import multiprocessing as mp
+# https://docs.python.org/3/library/multiprocessing.html
+import random
 import time
-import math
-from collections import deque
 
-def memo(memory, strategy):
-    for food, money in memory:
-        strategy[food] = money
+def make_episodes(episode_num, info):
+    """
+    複数のエピソードを作成し、データを取得
+
+    Parameters
+    ----------
+    episode_num : int
+        生成するエピソードの個数
+    info : dict
+        重要な情報 ex NNの重みなど
+
+    Returns
+    -------
+    output_list : list
+        エピソードのデータ
+    """
+    output_list = []
+    for _ in range(episode_num):
+        str_sequence = ""
+        num = 0
+        while random.choice([True, False]):
+            str_sequence += str(num)
+            num +=1
+        output_list.append(str_sequence)
+    return output_list
+
+
+def run_and_make_whether_need_episodes(q_in, q_out, episode_num, info):
+    """
+    合図が来たら、make_episodesを実行し、結果をqueueに渡す。
+
+    Parameters
+    ----------
+    q_in : queue
+        合図を送るqueue
+    q_out : queue
+        得られたデータを送るqueue
+    episode_num : int
+        生成するエピソードの個数
+    info : dict
+        重要な情報 ex NNの重みなど
+    """
+
+    while True:
+        whether_need_episode = q_in.get()
+        #プロセス終了
+        if whether_need_episode < 0:
+            break
+        #仕事start
+        episode_memory = make_episodes(episode_num, info)
+        q_out.put(episode_memory)
+
+
+def strategy_update(memory):
+    count = 0
+    for list_i in memory:
+        count += len(list_i)
+    print(count)
 
 
 
 if __name__ == '__main__':
-    # --  ver1 --
-    with Manager() as manager:
-        d = manager.dict()
-        M = [["apple", 100], ["banana", 200] ,["orange", 300]]
-        process = Process(target = memo, args=(M, d))
-        process.start()
-        process.join()
+    #q_in → 仕事startの合図, q_out → エピソードデータ貯める
+    q_out, q_in = mp.Queue(), mp.Queue()
+    episode_num = 10
 
-        print(d)
+    #info → processにうけわたす必要のあるもの、NNの重みなど？
+    info = ""
+    p = mp.Process(target=run_and_make_whether_need_episodes, args=(q_in, q_out, episode_num, info))
+    p.start()
+
+    #エピソード作成と戦略更新
+    iteration = 10
+    Memory = []
+    for _ in range(iteration):
+
+        #エピソード作成
+        x = 1
+        q_in.put(x)
+
+        Memory.append(q_out.get())
+
+        #戦略更新
+        strategy_update(Memory)
+
+
+    #Process終了の合図
+    x = -1
+    q_in.put(x)
+    p.join()
